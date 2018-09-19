2d71fdfaa14b6ed3dece94ed26bccc625c533ada
rleongPd5 <rleong00@stuy.edu>
2018-09-19 01:32:49 -0400

    Added CtoF fxn with test cases

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,4 +1,11 @@
-public class CtoFTester{
+public class CtoFTester{ //Class must be the same name as the file name
   public static void main(String[] args) {
+    //Test Cases for CtoF
+    System.out.println(CtoF(100));
+    System.out.println(CtoF(0));
+    System.out.println(CtoF(53));
+  }
+  public static double CtoF(int x){
+    return (x * 9.0 / 5) + 32;
   }
 }
