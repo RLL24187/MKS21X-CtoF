5c4b3d27f584f560a10618f5125b29bb6d85d6df
rleongPd5 <rleong00@stuy.edu>
2018-09-19 01:36:06 -0400

    Added comments with answers to test cases

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,9 +1,9 @@
 public class CtoFTester{ //Class must be the same name as the file name
   public static void main(String[] args) {
     //Test Cases for CtoF
-    System.out.println(CtoF(100));
-    System.out.println(CtoF(0));
-    System.out.println(CtoF(53));
+    System.out.println(CtoF(100)); //212.0
+    System.out.println(CtoF(0)); //32.0
+    System.out.println(CtoF(53)); //127.4
   }
   public static double CtoF(int x){
     return (x * 9.0 / 5) + 32;
